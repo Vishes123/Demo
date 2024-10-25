f2bcfa1e4c95b5fc7c4da865896c8501de73f3b1
Vishes123 <visheshkhare85@gmail.com>
2024-10-25 12:06:35 +0530

    initial commit

---
+++ Letsgit/Sum.java
@@ -0,0 +1,19 @@
+package Letsgit;
+
+/**
+ * Sum
+ */
+public class Sum {
+public static void main(String[] args) {
+    int N1 =10 , N2=20;
+    int tamp;
+    System.out.println("Before Swaping "+N1+","+N2);
+
+    tamp =N1;
+    N1=N2;
+    N2=tamp;
+    System.out.println("After Swaping "+N1+","+N2);
+
+}
+    
+}
